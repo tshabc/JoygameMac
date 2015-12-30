4b295554cf0d80cd7adcc72defed639a74a61a15
Shunhua Tang <tshabc87@163.com>
2015-12-30 11:13:56 +0800

    test

---
+++ cppProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cppProject
+//
+//  Created by 唐顺华 on 15/12/30.
+//  Copyright © 2015年 Hua. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
